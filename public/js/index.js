ffe07558ecf7153ab4a9418ef15b36740cc582ae
abdurrahmansaber <abdelrahmansaber85@gmail.com>
2023-06-15 00:39:45 +0300

    removed unnecessary edit from this branch

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,3 @@
 document.getElementById("add-product-btn").onclick = function () {
     location.href = "add-product.php";
 };
-
-const container = document.getElementById('container');
-
-container.addEventListener('click', function (event) {
-    const clickedDiv = event.target.closest('.clickable-div');
-    if (clickedDiv) {
-        const divId = clickedDiv.getAttribute('data-id');
-        const divProductType = clickedDiv.getAttribute('data-type');
-        window.location.href = `edit.php?id=${divId}&type=${divProductType}`;
-    }
-});
